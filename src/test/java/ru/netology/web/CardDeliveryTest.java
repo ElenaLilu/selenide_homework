75d1e61fa825c9eaff69360e3580d66bfcd62289
ElenaLilu <elena.imyarekova1@gmail.com>
2023-09-29 21:00:15 +0300

    src

--- src/test/java/ru/netology/web/CardDeliveryTest.java
+++ src/test/java/ru/netology/web/CardDeliveryTest.java
@@ -1,8 +1,11 @@
 package ru.netology.web;
 
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 
 import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Condition.visible;
@@ -11,17 +14,22 @@ import static com.codeborne.selenide.Selenide.*;
 
 public class CardDeliveryTest {
 
+    String generateDate(int days, String pattern) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern(pattern));
+    }
+
     @Test
-    void shouldTest() {
+    void cardDelivery() {
         open("http://localhost:9999");
-        $("[name='city']").setValue("Москва");
-        $("[name='date']").setValue("04.10.2023");
-        $("[name='name']").setValue("Мария Иванова");
-        $("[name='phone']").setValue("+79573452122");
-        $("[name='agreement']").click();
-        $$("button").find(exactText("Продолжить")).click();
+        $("[data-test-id='city'] input").setValue("Москва");
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT,Keys.HOME),Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").setValue("10.10.2023");
+        $("[data-test-id='name'] input").setValue("Мария Иванова");
+        $("[data-test-id='phone'] input").setValue("+79573452122");
+        $("[data-test-id='agreement']").click();
+        $$("button").find(exactText("Забронировать")).click();
         $(withText("Встреча успешно забронирована"))
-                .shouldBe(visible, Duration.ofSeconds(10));
+                .shouldBe(visible, Duration.ofSeconds(15));
 
     }
 }

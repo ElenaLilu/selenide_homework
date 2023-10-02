34b214adaa18cbefe342d03149995922f38fb808
ElenaLilu <elena.imyarekova1@gmail.com>
2023-10-02 10:07:34 +0300

    test update

--- src/test/java/ru/netology/web/CardDeliveryTest.java
+++ src/test/java/ru/netology/web/CardDeliveryTest.java
@@ -8,9 +8,7 @@ import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
-import static com.codeborne.selenide.Condition.exactText;
-import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
 
 public class CardDeliveryTest {
@@ -25,13 +23,14 @@ public class CardDeliveryTest {
         String date = generateDate(3, "dd.MM.yyyy");
         $("[data-test-id='city'] input").setValue("Москва");
         $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT,Keys.HOME),Keys.BACK_SPACE);
-        $("[data-test-id='date'] input").setValue("10.10.2023");
+        $("[data-test-id='date'] input").sendKeys(date);
         $("[data-test-id='name'] input").setValue("Мария Иванова");
         $("[data-test-id='phone'] input").setValue("+79573452122");
         $("[data-test-id='agreement']").click();
         $$("button").find(exactText("Забронировать")).click();
-        $(withText("Встреча успешно забронирована"))
-                .shouldBe(visible, Duration.ofSeconds(15));
+        $(".notification__content")
+                .shouldHave(text("Встреча успешно забронирована на " + date), Duration.ofSeconds(15))
+                .shouldBe(visible);
 
     }
 }
